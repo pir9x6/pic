1bfbddfdbd347dc4a3f5b2c48f79a2fd049bcf95
pir9x6 <pierre.blache.54@gmail.com>
2026-07-28 23:36:03 +0200

    formatting

--- libraries/lib_comp/tmp75.h
+++ libraries/lib_comp/tmp75.h
@@ -1,14 +1,23 @@
 #ifndef LIB_COMP_TMP75_H
 #define LIB_COMP_TMP75_H
 
+/*****************************************************************************
+* Includes
+******************************************************************************/
 #include "types.h"
 #include "i2c.h"
 
+/*****************************************************************************
+* Defines
+******************************************************************************/
 #define TMP75_REG_TEMPERATURE       0x00
 #define TMP75_REG_CONFIGURATION     0x01
 #define TMP75_REG_LIMIT_LOW         0x02
 #define TMP75_REG_LIMIT_HIGH        0x03
 
+/*****************************************************************************
+* Prototypes
+******************************************************************************/
 result_t tmp75_read_temp(I2C_BUS bus_id, u8 dev_addr, float *temp);
 result_t tmp75_configure(I2C_BUS bus_id, u8 dev_addr);
 
