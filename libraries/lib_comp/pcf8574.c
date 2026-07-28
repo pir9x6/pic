8375f66329360a0984b35df224c7d182f4f4943c
pir9x6 <pierre.blache.54@gmail.com>
2026-07-28 22:48:09 +0200

    Write & Read validated

--- libraries/lib_comp/pcf8574.c
+++ libraries/lib_comp/pcf8574.c
@@ -1,20 +1,33 @@
+/*****************************************************************************
+*   v1.0    2026-07-28  Original version (read & write validated)
+******************************************************************************/
+
+/*****************************************************************************
+* Includes
+******************************************************************************/
 #include "i2c.h"
 #include "pcf8574.h"
 #include "types.h"
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//---------------------- Write on the port of PCF8574 -------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+/*****************************************************************************
+* Write on the port of PCF8574
+******************************************************************************/
 result_t pcf8574_write_port (I2C_BUS i2c_bus_id, u8 dev_addr, u8 data)
 {
     if (i2c_start(i2c_bus_id) != SUCCESS)
         return ERROR;
 
-    if (i2c_write(i2c_bus_id, (dev_addr<<1) & 0xFE) != SUCCESS)
+    if (i2c_write(i2c_bus_id, I2C_ADDR_WR(dev_addr)) != SUCCESS)
+    {
+        i2c_stop(i2c_bus_id);
         return ERROR;
+    }
 
     if (i2c_write(i2c_bus_id, data) != SUCCESS)
+    {
+        i2c_stop(i2c_bus_id);
         return ERROR;
+    }
 
     if (i2c_stop(i2c_bus_id) != SUCCESS)
         return ERROR;
@@ -22,20 +35,25 @@ result_t pcf8574_write_port (I2C_BUS i2c_bus_id, u8 dev_addr, u8 data)
     return SUCCESS;
 }
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//---------------------- Read from the port of PCF8574 ------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+/*****************************************************************************
+* Read from the port of PCF8574
+******************************************************************************/
 result_t pcf8574_read_port (I2C_BUS i2c_bus_id, u8 dev_addr, u8 *data)
 {
-    // TO Do !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if (i2c_start(i2c_bus_id) != SUCCESS)
         return ERROR;
 
-    if (i2c_write(i2c_bus_id, (dev_addr<<1) & 0xFE) != SUCCESS)
+    if (i2c_write(i2c_bus_id, I2C_ADDR_RD(dev_addr)) != SUCCESS)
+    {
+        i2c_stop(i2c_bus_id);
         return ERROR;
+    }
 
     if (i2c_read(i2c_bus_id, I2C_NACK, data) != SUCCESS)
+    {
+        i2c_stop(i2c_bus_id);
         return ERROR;
+    }
 
     if (i2c_stop(i2c_bus_id) != SUCCESS)
         return ERROR;
