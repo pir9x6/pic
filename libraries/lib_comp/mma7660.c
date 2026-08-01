4014a6fd3c4f4396755aa19756f878eed4816814
pir9x6 <pierre.blache.54@gmail.com>
2026-08-01 16:35:08 +0200

    added acceleration reading

--- libraries/lib_comp/mma7660.c
+++ libraries/lib_comp/mma7660.c
@@ -1,7 +1,24 @@
+/*****************************************************************************
+*   v1.0    2026-07-28  Original version (read validated)
+******************************************************************************/
+
+/*****************************************************************************
+* Includes
+******************************************************************************/
 #include "i2c.h"
 #include "mma7660.h"
 #include "types.h"
 
+/*****************************************************************************
+* Docs
+******************************************************************************/
+// https://www.nxp.com/docs/en/data-sheet/MMA7660FC.pdf
+
+static result_t _read_raw   (I2C_BUS i2c_bus_id, u8 dev_addr, s8 *x, s8 *y, s8 *z);
+
+/*****************************************************************************
+* Constants
+******************************************************************************/
 const s8 MMA_XY[64] = {
       0,   3,   5,   8,  11,  14,  16,  19,
      22,  25,  28,  31,  34,  38,  41,  45,
@@ -24,50 +41,110 @@ const s8 MMA_Z[64]  = {
     -68, -71, -74, -76, -79, -82, -85, -87
 };
 
-
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//------------------------------- Initialization ------------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-result_t mma7660_init (I2C_BUS i2c_bus_id)
+/*****************************************************************************
+* Initialize the sensor
+******************************************************************************/
+result_t mma7660_init (I2C_BUS i2c_bus_id, u8 dev_addr)
 {
     /* stby mode */
-    if (i2c_write_reg (i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_MODE, 0x00) != SUCCESS)
+    if (i2c_write_reg (i2c_bus_id, dev_addr, MMA7660_REG_MODE, MMA7660_MODE_STBY) != SUCCESS)
         return ERROR;
 
     /* no interrupts */
-    if (i2c_write_reg (i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_INT,  0x00) != SUCCESS)
+    if (i2c_write_reg (i2c_bus_id, dev_addr, MMA7660_REG_INT,  0x00) != SUCCESS)
         return ERROR;
 
     /* sampling rate = 120sps */
-    if (i2c_write_reg (i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_SR,   0x00) != SUCCESS)
-        return ERROR;    
+    if (i2c_write_reg (i2c_bus_id, dev_addr, MMA7660_REG_SR,   MMA7660_SR_120_SPS) != SUCCESS)
+        return ERROR;
 
     /* enable accelorometer */
-    if (i2c_write_reg (i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_MODE, 0x01) != SUCCESS)
+    if (i2c_write_reg (i2c_bus_id, dev_addr, MMA7660_REG_MODE, MMA7660_MODE_ACTIVE) != SUCCESS)
         return ERROR;
 
     return SUCCESS;
 }
 
+/*****************************************************************************
+* Get X, Y, Z acceleration values from sensor
+******************************************************************************/
+static result_t _read_raw (I2C_BUS i2c_bus_id, u8 dev_addr, s8 *x, s8 *y, s8 *z)
+{
+    u8 raw_x;
+    u8 raw_y;
+    u8 raw_z;
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//---------------------------------- Get data ---------------------------------
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-result_t mma7660_read_axis (I2C_BUS i2c_bus_id, s8 *X, s8 *Y, s8 *Z)
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_X, &raw_x) != SUCCESS)
+        return ERROR;
+
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_Y, &raw_y) != SUCCESS)
+        return ERROR;
+
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_Z, &raw_z) != SUCCESS)
+        return ERROR;
+
+    /* Sign extension from 6 bits to 8 bits */
+    if (raw_x & 0x20)
+        raw_x |= 0xC0;
+    else
+        raw_x &= 0x3F;
+
+    if (raw_y & 0x20)
+        raw_y |= 0xC0;
+    else
+        raw_y &= 0x3F;
+
+    if (raw_z & 0x20)
+        raw_z |= 0xC0;
+    else
+        raw_z &= 0x3F;
+
+    *x = (s8)raw_x;
+    *y = (s8)raw_y;
+    *z = (s8)raw_z;
+
+    return SUCCESS;
+}
+
+result_t mma7660_read_accel(I2C_BUS i2c_bus_id, u8 dev_addr, float *x, float *y, float *z)
+{
+    s8 raw_x;
+    s8 raw_y;
+    s8 raw_z;
+
+    if (_read_raw(i2c_bus_id, dev_addr, &raw_x, &raw_y, &raw_z) != SUCCESS)
+    {
+        return ERROR;
+    }
+
+    *x = (float)raw_x * MMA7660_SENSITIVITY;
+    *y = (float)raw_y * MMA7660_SENSITIVITY;
+    *z = (float)raw_z * MMA7660_SENSITIVITY;
+
+    return SUCCESS;
+}
+
+/*****************************************************************************
+* Get angles from sensor
+******************************************************************************/
+result_t mma7660_read_angles (I2C_BUS i2c_bus_id, u8 dev_addr, s8 *x, s8 *y, s8 *z)
 {
     u8 tmp = 0;
 
-    if (i2c_read_reg(i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_X, &tmp) != SUCCESS)
+    if ((x == NULL) || (y == NULL) || (z == NULL))
         return ERROR;
-    *X = MMA_XY[tmp];
 
-    if (i2c_read_reg(i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_Y, &tmp) != SUCCESS)
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_X, &tmp) != SUCCESS)
         return ERROR;
-    *Y = MMA_XY[tmp];
+    *x = MMA_XY[tmp];
 
-    if (i2c_read_reg(i2c_bus_id, I2C_ADR_MMA7660, MMA7660_REG_Z, &tmp) != SUCCESS)
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_Y, &tmp) != SUCCESS)
         return ERROR;
-    *Z = MMA_Z [tmp];
+    *y = MMA_XY[tmp];
+
+    if (i2c_read_reg(i2c_bus_id, dev_addr, MMA7660_REG_Z, &tmp) != SUCCESS)
+        return ERROR;
+    *z = MMA_Z [tmp];
 
     return SUCCESS;
 }
--- libraries/lib_comp/mma7660.h
+++ libraries/lib_comp/mma7660.h
@@ -1,22 +1,53 @@
 #ifndef LIB_COMP_MAX7660_H
 #define LIB_COMP_MAX7660_H
 
+/*****************************************************************************
+* Includes
+******************************************************************************/
 #include "i2c.h"
 #include "types.h"
 
-#define MMA7660_REG_X           0x00
-#define MMA7660_REG_Y           0x01
-#define MMA7660_REG_Z           0x02
-#define MMA7660_REG_TILT        0x03
-#define MMA7660_REG_RATE        0x04
-#define MMA7660_REG_SLEEP       0x05
-#define MMA7660_REG_INT         0x06
-#define MMA7660_REG_MODE        0x07
-#define MMA7660_REG_SR          0x08
-#define MMA7660_REG_TAP         0x09
-#define MMA7660_REG_TAP_CNT     0x0A
+/*****************************************************************************
+* Registers
+******************************************************************************/
+#define MMA7660_REG_X               0x00
+#define MMA7660_REG_Y               0x01
+#define MMA7660_REG_Z               0x02
+#define MMA7660_REG_TILT            0x03
+#define MMA7660_REG_RATE            0x04
+#define MMA7660_REG_SLEEP           0x05
+#define MMA7660_REG_INT             0x06
+#define MMA7660_REG_MODE            0x07
+#define MMA7660_REG_SR              0x08
+#define MMA7660_REG_TAP             0x09
+#define MMA7660_REG_TAP_CNT         0x0A
 
-result_t mma7660_init     (I2C_BUS i2c_bus_id);
-result_t mma7660_read_axis(I2C_BUS i2c_bus_id, s8 *x, s8 *y, s8 *z);
+/*****************************************************************************
+* Definitions
+******************************************************************************/
+#define MMA7660_MODE_STBY           0x00
+#define MMA7660_MODE_ACTIVE         0x01
+#define MMA7660_MODE_AUTO_WAKE      0x02
+#define MMA7660_MODE_AUTO_SLEEP     0x04
+#define MMA7660_MODE_PRESCALER      0x10
+#define MMA7660_MODE_INT_TYPE       0x20
+#define MMA7660_MODE_IPP            0x40
+#define MMA7660_MODE_IAH            0x80
+
+#define MMA7660_SR_120_SPS          0x00
+#define MMA7660_SR_64_SPS           0x01
+#define MMA7660_SR_32_SPS           0x02
+#define MMA7660_SR_16_SPS           0x03
+
+#define MMA7660_RANGE_G             3.0f
+#define MMA7660_COUNTS              64.0f
+#define MMA7660_SENSITIVITY         (MMA7660_RANGE_G / MMA7660_COUNTS)
+
+/*****************************************************************************
+* Prototypes
+******************************************************************************/
+result_t mma7660_init       (I2C_BUS i2c_bus_id, u8 dev_addr);
+result_t mma7660_read_angles(I2C_BUS i2c_bus_id, u8 dev_addr, s8 *x, s8 *y, s8 *z);
+result_t mma7660_read_accel (I2C_BUS i2c_bus_id, u8 dev_addr, float *x, float *y, float *z);
 
 #endif
