e771dea259a5340349185ea00aa97aea6ceea33a
pir9x6 <pierre.blache.54@gmail.com>
2026-08-14 18:40:29 +0200

    Added reference files for interrupt vectors

---
+++ libraries/lib_pic/interrupt_vectors/interrupt_vectors_PIC33FJ128MC802.c
@@ -0,0 +1,233 @@
+// Microchip/MPLABX/v6.20/packs/Microchip/dsPIC33F-GP-MC_DFP/1.4.235/xc16/docs/vector_docs
+
+void __attribute__((interrupt, no_auto_psv)) _OscillatorFail(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _AddressError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _StackError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MathError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMACError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA0Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI1ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SI2C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MI2C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CMPInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC7Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC8Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T5Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2RXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2TXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI2ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1RxRdyInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _PMPInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MPWM1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _QEI1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA5Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _RTCCInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _FLTA1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CRCInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA6Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA7Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1TxReqInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MPWM2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _FLTA2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _QEI2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DAC1RInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DAC1LInterrupt(void)
+{
+}
---
+++ libraries/lib_pic/interrupt_vectors/interrupt_vectors_PIC33FJ256MC710.c
@@ -0,0 +1,301 @@
+// Microchip/MPLABX/v6.20/packs/Microchip/dsPIC33F-GP-MC_DFP/1.4.235/xc16/docs/vector_docs
+
+void __attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _ADC2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _AddressError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1RxRdyInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C1TxReqInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C2RxRdyInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _C2TxReqInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CMPInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CNInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _CRCInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMACError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA0Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA5Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA6Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _DMA7Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _FLTAInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _FLTBInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC5Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC6Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC7Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _IC8Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _INT4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MathError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MI2C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _MI2C2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC1Interrupt(void)
+{
+    _OC1IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC2Interrupt(void)
+{
+    _OC2IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC3Interrupt(void)
+{
+    _OC3IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC4Interrupt(void)
+{
+    _OC4IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC5Interrupt(void)
+{
+    _OC5IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC6Interrupt(void)
+{
+    _OC6IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC7Interrupt(void)
+{
+    _OC7IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OC8Interrupt(void)
+{
+    _OC8IF = 0;
+}
+
+void __attribute__((interrupt, no_auto_psv)) _OscillatorFail(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _PMPInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _PWMInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _QEIInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SI2C1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SI2C2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI1ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI2ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _SPI2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _StackError(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T2Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T3Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T4Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T5Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T6Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T7Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T8Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _T9Interrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2ErrInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2RXInterrupt(void)
+{
+}
+
+void __attribute__((interrupt, no_auto_psv)) _U2TXInterrupt(void)
+{
+}
